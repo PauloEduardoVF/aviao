635efbfa23ebb053399aea70e456d3fa966311e0
PauloEduardoVF <pauloeduardo.vf@gmai.com>
2026-06-17 20:13:46 -0300

    Alterando a cor do fundo para cinza e colocando o venv para funcionar

--- avioes.py
+++ avioes.py
@@ -1,8 +1,26 @@
 import pygame
 import sys
-
+from config import Config
 
 class Avioes:
     def __init__(self):
         pygame.init()
-    
+        self.frame = pygame.time.Clock() 
+        
+        self.config = Config()
+        self.tela = pygame.display.set_mode((self.config.tela_largura, self.config.tela_altura))
+        pygame.display.set_caption("Aviões!")
+        self.cor_fundo = self.config.cor
+
+    def rodar_jogo(self):
+        while True:
+            for eventos in pygame.event.get():
+                if eventos.type == pygame.QUIT:
+                    sys.exit()
+            pygame.display.flip()
+            self.frame.tick(60)
+            self.tela.fill(self.cor_fundo)
+
+if __name__ == "__main__":
+    jogo = Avioes()
+    jogo.rodar_jogo()
---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    def __init__(self):
+        self.tela_largura = 1200
+        self.tela_altura = 800
+        self.cor = (230,230, 230)
